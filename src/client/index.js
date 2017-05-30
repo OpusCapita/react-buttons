99564b052ceafae70548af9123ea5795a36d5263
Kirill Volkovich <volkovich@scand.com>
2017-05-30 16:10:36 +0300

    Fix package exports

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 // Here should be es2016 or CommonJS exports.
 
 module.exports = {
-  Button: require('./components/Button'),
-  TitledButton: require('./components/TitledButton')
+  Button: require('./components/Button').default,
+  TitledButton: require('./components/TitledButton').default
 };
