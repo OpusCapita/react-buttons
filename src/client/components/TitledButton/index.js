65b428c662f7a73d9579336f81c10bc359d6c7e4
Dmitry Shienok <dshienok@scand.com>
2018-04-25 11:28:00 +0300

    Created new release branch. [ci skip]

--- src/client/components/TitledButton/index.js
+++
@@ -1,1 +0,0 @@
-export default require('./TitledButton.react').default;
--- src/client/index.js
+++
@@ -1,6 +0,0 @@
-// Here should be es2016 or CommonJS exports.
-
-module.exports = {
-  Button: require('./components/Button').default,
-  TitledButton: require('./components/TitledButton').default
-};
